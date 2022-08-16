dee59680ecaaed14a72430fe25029edceb9fba56
aungkhantxenera <johnny.zaw@xenera.jp>
2022-08-16 22:12:14 +0630

    fixed

---
+++ app/Http/Controllers/ExpandController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Expand;
+use App\Http\Requests\StoreExpandRequest;
+use App\Http\Requests\UpdateExpandRequest;
+
+class ExpandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreExpandRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreExpandRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Expand  $expand
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Expand $expand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Expand  $expand
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Expand $expand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateExpandRequest  $request
+     * @param  \App\Models\Expand  $expand
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateExpandRequest $request, Expand $expand)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Expand  $expand
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Expand $expand)
+    {
+        //
+    }
+}
---
+++ database/seeders/ExpandSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ExpandSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
